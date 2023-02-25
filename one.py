03d6490e88299316c2b0ab66b236885e616aa27c
anupjani <126309524+anupjani@users.noreply.github.com>
2023-02-26 07:25:44 +1100

    Just added one new sample python file.

---
+++ one.py
@@ -0,0 +1,13 @@
+# Take input character from user.
+var=input("Enter a character: ")
+
+# Take input number from user.
+num=int(input("Enter number: "))
+
+# Empty variable string.
+var2=""
+
+# Looping
+for ctr in range(num):
+        var2=var2+var
+        print(var2)
